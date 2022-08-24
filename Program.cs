2b95176f1ea4d01df618ac5f018274e7919b9a23
Anton <zoveza268@gmail.com>
2022-08-24 23:15:45 +0500

    Homework 5

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@
 //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 // Console.Write("Введите трехзначное число: ");
 // int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-// 
+// string stringNumber = Convert.ToString(threeDigitNumber);
 // Console.WriteLine("Вторая цифра этого числа: "+stringNumber[1]);
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
@@ -169,3 +169,124 @@
 // Console.WriteLine("Cумма всех цифр в числе = " + sum);
 
 
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Сгенерированный массив: ");
+// PrintArray(numbers);
+// int count = 0;
+
+// for (int z = 0; z < numbers.Length; z++)
+// if (numbers[z] % 2 == 0)
+// count++;
+
+// Console.WriteLine($"{numbers.Length} чисел в массиве, {count} из них чётные");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//      Console.Write("[");
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i]);
+//         if (i != numbers.Length - 1 )
+//         {
+//             Console.Write(",");
+//         }
+
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Массив: ");
+// PrintArray(numbers);
+// int sum = 0;
+
+// for (int z = 1; z < numbers.Length; z+=2)
+//     sum = sum + numbers[z];
+
+//     Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = new Random().Next(1,10);
+//         }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//                 Console.Write(numbers[i]);
+//         if (i != numbers.Length - 1 )
+//         {
+//             Console.Write(",");
+//         }
+
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// double[] numbers = new double[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Вот наш массив: ");
+// PrintArray(numbers);
+// double min = Int32.MaxValue;
+// double max = Int32.MinValue;
+
+// for (int z = 0; z < numbers.Length; z++)
+// {
+//     if (numbers[z] > max)
+//         {
+//             max = numbers[z];
+//         }
+//     if (numbers[z] < min)
+//         {
+//             min = numbers[z];
+//         }
+// }
+
+// Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+// Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+// void FillArrayRandomNumbers(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+//         }
+// }
+// void PrintArray(double[] numbers)
+// {
+//     Console.Write("[");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
