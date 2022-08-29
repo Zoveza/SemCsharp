7c419d49fb8e0a08dbc60d2cc267160622c60c1e
Anton <zoveza268@gmail.com>
2022-08-30 01:37:37 +0500

    Homework 6

--- Program.cs
+++ Program.cs
@@ -290,3 +290,37 @@
 //     Console.Write("]");
 //     Console.WriteLine();
 // }
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// Console.Write($"Введите число М(количество чисел): ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] massiveNumbers = new int[m];
+
+// void InputNumbers(int m){
+// for (int i = 0; i < m; i++)
+//   {
+//  Console.Write($"Введите {i+1} число: ");
+//  massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+//   }
+// }
+// int Comparison(int[] massiveNumbers)
+// {
+//  int count = 0;
+//  for (int i = 0; i < massiveNumbers.Length; i++)
+//   {
+//  if(massiveNumbers[i] > 0 ) count += 1; 
+//   }
+//  return count;
+// }
+// InputNumbers(m);
+// Console.WriteLine($"Введено {Comparison(massiveNumbers)} чисел больше 0 ");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
